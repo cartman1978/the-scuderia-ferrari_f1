de31337804b4ff430d5adafeed0d8e789abbfa60
cartman1978 <cartman_7802@yahoo.ie>
2021-06-15 19:57:59 +0000

    Add cars.html and connect to mongodb

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello world .... Santarnell"
+@app.route("/get_cars")
+def get_cars():
+    cars = mongo.db.cars.find()
+    return render_template("cars.html", cars=cars)
 
 
 if __name__ == "__main__":
