389c1fd6e9a0749ca5106bd424e0dad91f67c285
Fabio Torti <cartman_7802@yahoo.ie>
2021-07-18 11:31:27 +0100

    add materialize that fits better with thestyle in mind

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,7 @@
-
 $(document).ready(function () {
-    // Code from: https://bulma.io/documentation/components/navbar/
+    $('.modal').modal();
 
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function () {
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-    });
 
-    // Modal for delete functions
-    // https://stackoverflow.com/questions/27650584/multiple-modals#27650708
-    $(".button").click(function () {
-        $(".modal").removeClass("is-active");
-        var Type = $(this).data("modal-type");
-        $("#" + Type).addClass("is-active");
-    });
 });
 
 
@@ -24,7 +10,10 @@ $(document).ready(function () {
         $('.hidden').hide();
         $(this).next().toggle();
     });
+
 });
+
+
 
 
 
