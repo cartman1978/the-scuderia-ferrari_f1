97ba4afecae89aea59e2e8f6ca0c0ba6df6bfc69
Fabio Torti <cartman_7802@yahoo.ie>
2021-08-05 19:18:46 +0100

    Add debug True

--- app.py
+++ app.py
@@ -235,4 +235,4 @@ def something_went_wrong(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
